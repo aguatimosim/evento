3bc2298b9b550d5fc1786f3eeaabb750f48b3951
Antonio Guatimosim <antonio.guatimosim@gmail.com>
2026-09-02 17:46:03 -0300

    first commit

---
+++ src/test/java/com/aguatimosim/Evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguatimosim.Evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
